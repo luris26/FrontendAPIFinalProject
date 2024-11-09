ec963242ebff5212aac94c7ff8c219d33c56a051
unknown <solisluris@gmail.com>
2024-11-08 18:29:44 -0600

    dockercompose

--- obj/Debug/net8.0/ServerPupusas.AssemblyInfo.cs
+++ obj/Debug/net8.0/ServerPupusas.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ServerPupusas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3cef51c3b5c1e97a5be938c9e9d1686b6fb5a7a3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d0dda6b3694d1abcbfce85df270dc45c4f3c86e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ServerPupusas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ServerPupusas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
